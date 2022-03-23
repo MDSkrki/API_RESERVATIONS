f695a315d2e9bacc78b34c7c301337f457f4a68b
Alejandro <Alex_bcn10@hotmail.es>
2022-03-23 18:24:00 +0100

    Express connection, port and mysql2 driver installed for sequelize

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 import express from "express";
+import env from "dotenv";
+
+const app = express();
+
+//Environment vars
+env.config();
+
+//Parse body to JSON
+app.use(express.json());
+
+// Express port definition and server up
+app.set("port", 4000);
+app.listen(app.get("port"), () => console.log("Server up at 4000"))
