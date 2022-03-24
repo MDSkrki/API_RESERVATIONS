ba067e3462b08979961fcf29a1d12b38386fa9a1
Alejandro <Alex_bcn10@hotmail.es>
2022-03-24 18:34:53 +0100

    create user and doctor model

--- config/MysqlDB.js
+++ config/MysqlDB.js
@@ -1,12 +1,15 @@
 import { Sequelize } from "sequelize";
 
+const connection = new Sequelize('api_reservations', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
 try{
-    const connection = new Sequelize('api_reservations', 'root', 'root', {
-        host: 'localhost',
-        dialect: 'mysql'
-    });
     await connection.authenticate();
     console.log('Connection has been established successfully.')
 }catch(error){
     console.log('Unable to connect to the database', error)
 }
+
+export default connection;
---
+++ src/doctor/doctorModel.js
@@ -0,0 +1,29 @@
+import { Sequelize, DataTypes, Model } from "sequelize";
+import connection from "../../config/mysqlDB.js"
+
+class Doctor extends Model {};
+
+const doctorSchema = {
+    // Model attributes are defined here
+    specialty: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    schedule: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    FK_idUser: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}
+
+Doctor.init(doctorSchema, {
+    // Other model options go here
+    sequelize: connection, // We need to pass the connection instance
+    modelName: 'Doctor' // We need to choose the model name
+  });
+await Doctor.sync();
+
+export default Doctor;
