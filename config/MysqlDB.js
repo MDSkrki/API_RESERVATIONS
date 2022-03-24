280118fee2c64677b47cd103ade8f4d6ce9236ab
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-24 21:24:12 +0100

    fixed db connection again after modifying exported items

--- config/MysqlDB.js
+++ config/MysqlDB.js
@@ -2,12 +2,12 @@ import { Sequelize } from "sequelize";
 import env from 'dotenv';
 env.config();
 
-const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+export const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
 });
 
-const dbConnection = async () => {
+export const dbConnection = async () => {
     try{
         await connection.authenticate();
         console.log('Connection has been established successfully.')
@@ -15,5 +15,3 @@ const dbConnection = async () => {
         console.log('Unable to connect to the database', error)
     }
 }
-
-export default dbConnection;
