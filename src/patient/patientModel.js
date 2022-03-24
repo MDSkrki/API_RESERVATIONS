a63c5eb394e117d6b8c680aa83d891b45fb5f63e
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-24 21:26:47 +0100

    created models for visits and patients

---
+++ src/patient/patientModel.js
@@ -0,0 +1,36 @@
+import { Model, DataTypes } from "sequelize";
+import { connection } from "../../config/mysqlDB.js";
+
+class Patient extends Model {}
+
+const patientSchema = {
+    sex: {
+        type: DataTypes.STRING,
+    },
+    birth_date: {
+        type: DataTypes.STRING,
+    },
+    age: {
+        type: DataTypes.INTEGER,
+    },
+    dni: {
+        type: DataTypes.STRING,
+    },
+    allergies: {
+        type: DataTypes.STRING,
+    },
+    address: {
+        type: DataTypes.STRING,
+    },
+    FK_idUser: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }, 
+}
+
+Patient.init(patientSchema, {
+    sequelize: connection,
+    modelName: 'Patient',
+});
+
+export default Patient;
---
+++ src/visit/visitModel.js
@@ -0,0 +1,33 @@
+import { DataTypes, Model } from "sequelize";
+import {connection} from "../../config/mysqlDB.js";
+
+class Visit extends Model {}
+
+const visitSchema = {
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    state: {
+        type: DataTypes.STRING,
+    },
+    FK_idDoctor: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    FK_idPatient: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }
+}
+
+Visit.init(visitSchema, {
+    sequelize: connection,
+    modelName: 'Visit',
+});
+
+export default Visit;
