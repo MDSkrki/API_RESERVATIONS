0ed86baa2981fa1236ad84b47abb33833ed3d99b
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-24 21:48:00 +0100

    created get and refactored post into controller through router for /user endpoint

--- src/users/userController.js
+++ src/users/userController.js
@@ -1,13 +1,17 @@
-import User from "./src/users/userModel.js";
+import User from "./userModel.js";
 
-// Post user
-app.post('/', async (req,res) => {
+export const getAllUsers = async (req, res) => {
+    const users = await User.findAll();
+    res.status(200).json(users);
+}
+
+export const postUser = async (req, res) => {
     const createUser = await User.create({
-            name: "mihai",
-            lastname: "daniel",
-            email: "mihai@api.com",
-            password: "mihai",
-            phone_number: "687543223"
-    })
-res.json(createUser)
-})
+        name: "mihai",
+        lastname: "daniel",
+        email: "mihai@api.com",
+        password: "mihai",
+        phone_number: "687543223"
+    });
+    res.json(createUser);
+}
--- src/users/userRoute.js
+++ src/users/userRoute.js
@@ -1,1 +1,13 @@
-const test = "testing fork";
+import express from "express";
+import * as controller from './userController.js'
+
+
+//Router initialisation
+const router = express.Router();
+
+router.get('/', controller.getAllUsers);
+
+// Post user
+router.post('/',controller.postUser);
+
+export default router;
