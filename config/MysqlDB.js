d0ae439f22e2796501f0f15f5222dd5a571c0765
AlexMonPe <alex_bcn10@hotmail.es>
2022-03-25 10:26:24 +0100

    configure env vars to deploy in heroku with clearDB mysql cloud

--- config/MysqlDB.js
+++ config/MysqlDB.js
@@ -3,7 +3,7 @@ import env from 'dotenv'
 env.config();
 
 const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+    host: CLEARDB_DATABASE_URL,
     dialect: process.env.DB_DIALECT,
 });
 
