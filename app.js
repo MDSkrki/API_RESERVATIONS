5677e038c185b9eaca6dc1c2ae2f765a82077d99
AlexMonPe <alex_bcn10@hotmail.es>
2022-03-25 11:06:15 +0100

    test deployed get doctors in app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import env from "dotenv";
 import {dbConnection} from "./config/mysqlDB.js";
+import Doctor from "./src/doctor/doctorModel.js"
 
 const app = express();
 
@@ -9,7 +10,7 @@ env.config();
 
 //Parse body to JSON
 app.use(express.json());
-app.get('/doctor',async (req,res) => {
+app.get('/doctor', async (req,res) => {
     const query = await Doctor.findAll({
         include: [{model: User}]
     })
