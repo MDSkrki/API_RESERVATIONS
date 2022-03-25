207ebae57343cb6ba7588022b98606e3191ce5b2
AlexMonPe <alex_bcn10@hotmail.es>
2022-03-25 10:55:03 +0100

    endpoint test deploy heroku

--- src/doctor/doctorController.js
+++ src/doctor/doctorController.js
@@ -1,7 +1,7 @@
 import Doctor from "./src/doctor/doctorModel.js";
 // Get doctor with users model
 
-app.get('/',async (req,res) => {
+app.get('/doctor',async (req,res) => {
     const query = await Doctor.findAll({
         include: [{model: User}]
     })
