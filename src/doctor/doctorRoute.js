9edd71df5d83d3aa39334ac8551ce407ff8c75e5
Alejandro <Alex_bcn10@hotmail.es>
2022-03-25 20:33:00 +0100

    crud users and start doctors crud

---
+++ src/doctor/doctorRoute.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+import { getDoctor, postDoctor, updateDoctor, deleteDoctor } from "./doctorController.js";
+
+//getUser
+router.get("/", );
+router.post("/", );
+router.patch("/:id", );
+router.delete("/:id", );
