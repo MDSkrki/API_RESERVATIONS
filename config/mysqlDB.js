4cebc11061a0eb7cc6fed3be5f806fc535f57577
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-25 20:38:29 +0100

    fixed db connection from pull and heroku experiments leftovers

--- config/mysqlDB.js
+++ config/mysqlDB.js
@@ -7,7 +7,7 @@ const connection = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
-    host: process.env.CLEARDB_DATABASE_URL,
+    host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   }
 );
