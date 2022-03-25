571201e0bd6f13579524a5906baf3986026a9f6e
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-25 21:58:49 +0100

    added query possibility to visit get

--- src/visit/visitController.js
+++ src/visit/visitController.js
@@ -1,4 +1,14 @@
-export const getVisits = (req, res) => {
-    console.log('Get to visits performed successfully');
-    res.json('Get to visits performed successfully');
+import Visit from "./visitModel.js";
+
+export const getVisits = async (req, res) => {
+    try {
+        const filter = req.query; 
+        const query = await Visit.findAll({
+            where: filter,
+        });
+    res.json(query);
+    } catch (error) {
+        console.log(error);
+        res.json(error);
+    }
 }
