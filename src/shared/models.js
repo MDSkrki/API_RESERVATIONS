be48d1e95d9681a8d220b420f30b00da4ae5ea5f
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-26 16:29:42 +0100

    completed assocations

--- src/shared/models.js
+++ src/shared/models.js
@@ -1,8 +1,22 @@
 import Doctor from "../doctor/doctorModel.js";
 import User from "../users/userModel.js";
+import Patient from '../patient/patientModel.js';
+import Visit from '../visit/visitModel.js';
 
 //Association 1:1 User with doctor
 User.hasOne(Doctor, { foreignKey: "FK_idUser" }),
 Doctor.hasOne(User, { foreignKey: "id" });
 
-export {User, Doctor};
+//Association 1:1 User with Patient
+User.hasOne(Patient);
+Patient.belongsTo(User);
+
+//Association 1:n Visit with Patient
+Patient.hasMany(Visit);
+Visit.belongsTo(Patient);
+
+//Assocation 1:n Visit with Doctor
+Doctor.hasMany(Visit);
+Visit.belongsTo(Doctor);
+
+export {User, Doctor, Visit, Patient};
