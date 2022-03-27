63dd096b8c6b88a70540d6c9c2f43c0841fac927
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-27 15:30:07 +0200

    remade models to avoid using userModel

--- src/doctor/doctorModel.js
+++ src/doctor/doctorModel.js
@@ -6,16 +6,38 @@ class Doctor extends Model {}
 
 const doctorSchema = {
   // Model attributes are defined here
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "doctor",
+  },
+  phone_number: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
   specialty: {
     type: DataTypes.STRING,
     allowNull: false,
   },
   schedule: {
     type: DataTypes.STRING,
-    allowNull: false,
-  },
-  idUser: {
-    type: DataTypes.INTEGER,
     allowNull: false,
   },
 };
@@ -25,8 +47,5 @@ Doctor.init(doctorSchema, {
   sequelize: connection, // We need to pass the connection instance
   modelName: "Doctor", // We need to choose the model name
 });
-
-
-
 
 export default Doctor;
--- src/patient/patientModel.js
+++ src/patient/patientModel.js
@@ -1,10 +1,35 @@
 import { Model, DataTypes } from "sequelize";
 import { connection } from "../../config/mysqlDB.js";
 
-
-class Patient extends Model {}
+class Patient extends Model { }
 
 const patientSchema = {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "patient",
+    },
+    phone_number: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
     sex: {
         type: DataTypes.STRING,
     },
@@ -23,17 +48,11 @@ const patientSchema = {
     address: {
         type: DataTypes.STRING,
     },
-    idUser: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    }, 
 }
 
 Patient.init(patientSchema, {
     sequelize: connection,
     modelName: 'Patient',
 });
-
-
 
 export default Patient;
