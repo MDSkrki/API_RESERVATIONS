3e2128690474fbedde2dfe81ca5c4f6517fb196a
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-29 09:53:06 +0200

    fixed authorisation middleware

--- src/shared/dbServices.js
+++ src/shared/dbServices.js
@@ -1,24 +1,9 @@
-import { Doctor, Patient, Visit } from "./models.js";
+import { User } from "./models.js";
 
-const logCheck = async (role, id) => {
-    if(role == 'patient') {
-        const patient = await Patient.findByPk(id);
-        if(patient.isLogged) return true
-    }
-    if(role == 'doctor') {
-        const doctor = await Doctor.findByPk(id);
-        if(doctor.isLogged) return true
-    }
-    return false
+const logCheck = async (id) => {
+    const user = await User.findByPk(id);
+    if (user.isLogged) return true;
+    return false;
 }
 
-const visitCreator = async (patient, doctor, body) => {
-    const visit = await Visit.create({
-        description: body.description,
-        state: body.state,
-        idDoctor: doctor.id,
-        idPatient: patient.id
-    })
-}
-
-export {logCheck, visitCreator}
+export { logCheck };
--- src/shared/middlewares.js
+++ src/shared/middlewares.js
@@ -1,20 +1,19 @@
-import { decode } from "jsonwebtoken";
 import { logCheck } from "./dbServices.js";
-import { tokenChecker } from "./services.js"
+import { tokenChecker } from "./services.js";
 
-const auth = (roleToCheck) => { 
-    return (req, res, next) => {
+const auth = (roleToCheck) => {
+    return async (req, res, next) => {
         try {
             const decoded = tokenChecker(req.headers.token, process.env.JWT_SECRET);
-            if(decoded.role === roleToCheck || decoded.role === 'Doctor') {
-                console.log(decoded);
-                if(logCheck(decoded.role, decoded.id)) {
+            if (decoded.role === roleToCheck || decoded.role === 'Doctor') {
+                if (await logCheck(decoded.id)) {
+                    //req.decoded = decoded;
                     next();
                 } else {
-                    res.json('You are not authorized');
-                }   
-            }else {
-                res.status(403).json('Unauthorized')
+                    res.json('You appear to be logged out. Please log in.');
+                }
+            } else {
+                res.status(403).json('Unauthorized');
             }
         } catch (error) {
             console.log(error);
@@ -23,4 +22,4 @@ const auth = (roleToCheck) => {
     }
 }
 
-export {auth}
+export { auth };
