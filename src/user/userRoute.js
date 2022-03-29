a6426e13e99926f18c637ebb810f793ca15e04d3
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-29 09:52:23 +0200

    added authorisation middlewares

--- src/user/userRoute.js
+++ src/user/userRoute.js
@@ -1,13 +1,14 @@
 import express from "express";
+import { auth } from "../shared/middlewares.js";
 const router = express.Router();
 import { getUser, postUser, updateUser, deleteUser,userLogin,userLogout } from "./userController.js";
 
 
-router.get("/", getUser); // DOCTOR
+router.get("/", auth('Doctor'), getUser); // Doctor
 router.get('/login', userLogin) // no auth
-router.get('/logout', userLogout) // auth DOCTOR Y Patient
-router.post("/", postUser); // doctor
-router.patch("/:id", updateUser); // only Doctors
-router.delete("/:id", deleteUser); // only doctors
+router.get('/logout', auth('Patient'), userLogout) // auth Doctor Y Patient
+router.post("/", auth('Doctor'), postUser); // Doctor
+router.patch("/:id", auth('Doctor'), updateUser); // only Doctors
+router.delete("/:id", auth('Doctor'), deleteUser); // only doctors
 
 export default router;
