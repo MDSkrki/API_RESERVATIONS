9818c32997492414a6c1eee457efb0146a3114fa
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-29 17:11:26 +0200

    fixed auth in Visits CRUD

--- src/visit/visitRoute.js
+++ src/visit/visitRoute.js
@@ -2,6 +2,8 @@ import express from "express";
 import { deleteVisit, getVisit, postVisit, updateVisit } from "./visitController.js";
 const router = express.Router();
 
+router.use(auth('Doctor'));
+
 router.get('/', getVisit); //Doctors can see all visits
 router.post('/', postVisit); // Doctors can create custom visits
 router.patch('/:id', updateVisit); // Doctors can change visit information
